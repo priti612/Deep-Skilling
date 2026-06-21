0d2a8a44fb66d869f3f3930a2774bb4eed7d45f7
priti612 <197772960+PritiPrabha@users.noreply.github.com>
2026-06-21 17:27:09 +0530

    Added Exercise 3

---
+++ Deepskilling/Engineering concepts/Algorithms_Data Structures/ex1.cs/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Product
+{
+    public int pid;
+    public string pname;
+    public int qty;
+    public double price;
+
+    public Product(int id, string n, int q, double p)
+    {
+        pid = id;
+        pname = n;
+        qty = q;
+        price = p;
+    }
+}
+
+class Program
+{
+    static Dictionary<int, Product> inv = new Dictionary<int, Product>();
+
+    static void Add(Product p)
+    {
+        inv[p.pid] = p;
+    }
+
+    static void Update(int id, int q, double p)
+    {
+        if (inv.ContainsKey(id))
+        {
+            inv[id].qty = q;
+            inv[id].price = p;
+        }
+    }
+
+    static void Delete(int id)
+    {
+        if (inv.ContainsKey(id))
+            inv.Remove(id);
+    }
+
+    static void Show()
+    {
+        foreach (var x in inv.Values)
+        {
+            Console.WriteLine(x.pid + " " + x.pname + " " + x.qty + " " + x.price);
+        }
+    }
+
+    static void Main()
+    {
+        Add(new Product(101, "Laptop", 20, 50000));
+        Add(new Product(102, "Mouse", 50, 500));
+
+        Console.WriteLine("After Add");
+        Show();
+
+        Update(101, 25, 52000);
+
+        Console.WriteLine("After Update");
+        Show();
+
+        Delete(102);
+
+        Console.WriteLine("After Delete");
+        Show();
+    }
+}
