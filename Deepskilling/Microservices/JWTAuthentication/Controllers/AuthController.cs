abd061cfb01d4c46c68bee59a636008867de5aae
priti612 <197772960+PritiPrabha@users.noreply.github.com>
2026-07-11 21:34:32 +0530

    Completed JWT Authentication Handson

---
+++ Deepskilling/Microservices/JWTAuthentication/Controllers/AuthController.cs
@@ -0,0 +1,53 @@
+﻿using JWTAuthentication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JWTAuthentication.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginModel model)
+        {
+            if (model.Username == "admin" && model.Password == "admin123")
+            {
+                var token = GenerateJwtToken(model.Username);
+
+                return Ok(new
+                {
+                    Token = token
+                });
+            }
+
+            return Unauthorized("Invalid Username or Password");
+        }
+
+        private string GenerateJwtToken(string username)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, username)
+            };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes("ThisIsASecretKeyForJwtToken123456789"));
+
+            var creds = new SigningCredentials(key,
+                SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "MyAuthServer",
+                audience: "MyApiUsers",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(60),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ Deepskilling/Microservices/JWTAuthentication/Models/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace JWTAuthentication.Models
+{
+    public class LoginModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
