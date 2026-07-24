724b5c6a6e1625e5180802298f6d21eb11269fda
priti612 <197772960+PritiPrabha@users.noreply.github.com>
2026-07-24 11:56:33 +0530

    Prepare Deepskilling/Upskilling content for main branch

--- Deepskilling/Deepskilling/React/handson2/src/components/About.js
+++
@@ -1,7 +0,0 @@
-function About() {
-    return (
-        <h2>Welcome to the About page of the Student Management Portal</h2>
-    );
-}
-
-export default About;
--- Deepskilling/Deepskilling/React/handson2/src/components/Home.js
+++
@@ -1,7 +0,0 @@
-function Home() {
-    return (
-        <h2>Welcome to the Home page of Student Management Portal</h2>
-    );
-}
-
-export default Home;
